366ca11185a2459451060dc7b0da39f61622843a
Subeen Park <psb021414@gmail.com>
2021-02-04 17:38:02 +0900

    form modified

--- phoing/myApp/forms.py
+++ phoing/myApp/forms.py
@@ -6,13 +6,13 @@ from django.contrib.auth import get_user_model
 class ProfileForm(UserCreationForm):
     class Meta:
         model = get_user_model()
-        fields = UserCreationForm.Meta.fields + ('first_name', 'last_name', 'email', 'category') +('image',)
-        
-    
+        fields = UserCreationForm.Meta.fields + \
+            ('first_name', 'last_name', 'email', 'category') + ('image',)
 
+    def __init__(self, *args, **kwargs):
+        super(ProfileForm, self).__init__(*args, **kwargs)
 
-
-
-
-
-
+        for field in self.fields.keys():
+            self.fields[field].widget.attrs.update({
+                'class': field + " form",
+                'id': 'form-id', })
