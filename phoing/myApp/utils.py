4cfa40f912317ffb3070f7c20b2763652abdf54c
seoljiwon <suljiwon@naver.com>
2021-02-14 21:00:54 +0900

    ajax incomplete

--- phoing/myApp/utils.py
+++ phoing/myApp/utils.py
@@ -8,7 +8,7 @@ import re
 from django.core.files import File
 from django.core.files.temp import NamedTemporaryFile
 
-from .models import Tag
+# from .models import Tag
 
 
 def uuid_name_upload_to(instance, filename):
@@ -69,5 +69,3 @@ def save_image_from_url(user, url):
 
     user.image.save(uuid_name_upload_to(user, user.email),
                     File(img_temp), save=True)
-
-
