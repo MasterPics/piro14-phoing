69edc774ee77b080a0be78b4d15d95cae1e332b3
Subeen Park <psb021414@gmail.com>
2021-02-20 22:24:48 +0900

    MESSAGE MODEL

--- phoing/chat/models.py
+++ phoing/chat/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from myApp.models import Contact
+from user.models import User
 
-# Create your models here.
+class Room(models.Model):
+    contact = models.OneToOneField(Contact, related_name='contact', on_delete=models.CASCADE)
+    is_closed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now=True)
+
+
+class Message(models.Model):
+    author = models.ForeignKey(User, related_name='author_messages', on_delete=models.SET_NULL)
+    room = models.ForeignKey(Room, related_name='room_messages', on_delete=models.CASCADE)    
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Participant(models.Model):
+    user = models.ForeignKey(User, related_name='user_participants', on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, related_name='room_participants', on_delete=models.CASCADE)
+    is_allowed = models.BooleanField(default=False) # 채팅방에 입장 허가 시 True
+    is_finished = models.BooleanField(default=False) # 채팅방에 참여중일 시 True
+
+
+    
+
+
+    
+
+
+
+
+
+
