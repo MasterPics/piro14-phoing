a3e2fe04c015271422249a950e10c2febafc4082
hongwj12 <hongwj12@snu.ac.kr>
2021-02-23 02:53:34 +0900

    merge ing----

--- websocket/alarms/routing.py
+++
@@ -1,5 +0,0 @@
-from . import consumers
-from django.urls import re_path
-websocket_urlpatterns = [
-    re_path(r'ws/test/(?P<username>.*)/', consumers.UserTestConsumer),
-]
--- websocket/websocket/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from alarms import views
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('ShareMe', views.ShareMe.as_view()),
-    path('Alarm', views.Alarm.as_view()),
-]
